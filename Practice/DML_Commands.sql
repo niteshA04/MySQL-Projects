bb2a37a96057f11956e699b08ba72151abaf75f3
Nitesh <89405710+niteshA04@users.noreply.github.com>
2022-06-29 02:26:05 +0530

    Basic, DDL and DML Commands

--- Practice/DML_Commands.sql
+++ Practice/DML_Commands.sql
@@ -1,84 +1,84 @@
--- # DML COMMANDS # --
+# DML COMMANDS # --
 
--- # Selecting Database
+# Selecting Database
 use abc_company;
 
--- # Creating a table
--- create table employees(
--- emp_id int not null,
--- first_name varchar(20),
--- last_name varchar(20),
--- salary int,
--- primary key(emp_id)
--- );
--- describe employees;
+# Creating a table
+create table employees(
+emp_id int not null,
+first_name varchar(20),
+last_name varchar(20),
+salary int,
+primary key(emp_id)
+);
+describe employees;
 
--- Adding primary key as I forgot to add in create table code
--- alter table employees
--- add primary key (emp_id);
+Adding primary key as I forgot to add in create table code
+alter table employees
+add primary key (emp_id);
 
--- # Inserting values in employees table
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (101, 'Steven', 'King', 1000);
+# Inserting values in employees table
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(101, 'Steven', 'King', 1000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (102, 'Edwin', 'Thomas', 2000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(102, 'Edwin', 'Thomas', 2000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (103, 'Harry', 'Potter', 3000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(103, 'Harry', 'Potter', 3000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (104, 'Steven', 'Potter', 4000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(104, 'Steven', 'Potter', 4000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (105, 'Edwin', 'Potter', 5000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(105, 'Edwin', 'Potter', 5000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (106, 'Harry', 'Thomas', 6000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(106, 'Harry', 'Thomas', 6000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (107, 'Joe', 'Biden', 7000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(107, 'Joe', 'Biden', 7000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (108, 'Chris', 'Potter', 8000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(108, 'Chris', 'Potter', 8000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (109, 'Joshep', 'Jhonson', 9000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(109, 'Joshep', 'Jhonson', 9000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (110, 'Liam', 'Potter', 10000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(110, 'Liam', 'Potter', 10000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (111, 'Haldte', 'Kildsa', 11000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(111, 'Haldte', 'Kildsa', 11000);
 
--- insert into employees
--- (emp_id, first_name, last_name, salary) values 
--- (112, 'Beuric', 'Haldne', 11000);
+insert into employees
+(emp_id, first_name, last_name, salary) values 
+(112, 'Beuric', 'Haldne', 11000);
 
--- describe employees;
--- select * from employees
+describe employees;
+select * from employees
 
--- # Updating data in employees table
--- update employees
--- set last_name = 'Cohen'
--- where emp_id=101;
--- select * from employees;
+# Updating data in employees table
+update employees
+set last_name = 'Cohen'
+where emp_id=101;
+select * from employees;
 
--- # Deleting Rows
--- delete from employees
--- where emp_id=112; 
--- select * from employees;
+# Deleting Rows
+delete from employees
+where emp_id=112; 
+select * from employees;
 
 
 
