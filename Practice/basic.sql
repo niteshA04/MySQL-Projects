f9c801e905dce8311c946b10f91411c5bf4b663f
Nitesh <89405710+niteshA04@users.noreply.github.com>
2022-06-30 16:13:01 +0530

    Regular Expression Added

--- Practice/basic.sql
+++ Practice/basic.sql
@@ -10,15 +10,48 @@ use abc_company;
 -- where salary  = 10000;
 
 -- # LOGICAL OPERATOR'S # --
-select * from employees
-where salary  = 6000 and first_name = 'Harry';
+-- select * from employees
+-- where salary  = 6000 and first_name = 'Harry';
 
-select * from employees
-where salary  = 6000 or first_name = 'Harry';
+-- select * from employees
+-- where salary  = 6000 or first_name = 'Harry';
 
-select * from employees
-where not salary = 6000;
+-- select * from employees
+-- where not salary = 6000;
 
+-- # COMPARISON OPERATOR'S # --
+-- select * from employees
+-- where salary = 6000;
+
+-- select * from employees
+-- where salary != 6000;
+
+-- select * from employees
+-- where salary < 6000;
+
+-- select * from employees
+-- where salary > 6000;
+
+-- select * from employees
+-- where salary >= 6000;
+
+-- select * from employees
+-- where not salary <= 6000;
+
+-- # SPECIAL OPERATOR'S # --
+-- select * from employees
+-- where salary between 6000 and 8000;
+
+-- select * from employees
+-- where salary like 6000;
+
+-- select * from employees
+-- where salary is null;
+
+-- select * from employees
+-- where salary in (6000,8000,10000);
+
+-- select distinct first_name from employees;
 
 
 
---
+++ Practice/regularExpression.sql
@@ -0,0 +1,38 @@
+# Using the database # --
+use abc_company;
+
+# Regular Expression # --
+select * from employees
+where last_name regexp 'j';
+
+select * from employees
+where last_name regexp '^p';
+
+select * from employees
+where last_name regexp 'on$';
+
+select * from employees
+where first_name regexp '[os]';
+
+select * from employees
+where first_name regexp 'Joe|Steven|Harry';
+
+select * from employees
+where last_name regexp '[a-h]e';
+
+select * from employees
+where first_name regexp 'J|Sen|rry';
+
+select * from employees
+where first_name regexp '^[li]';
+
+
+
+
+
+
+
+
+
+
+
