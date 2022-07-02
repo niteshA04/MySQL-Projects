d02d46e7c97d90833fbf5074d98e9d2c2a2c5cb7
Nitesh <89405710+niteshA04@users.noreply.github.com>
2022-07-02 15:01:33 +0530

    dartabase update

--- RNA_Bank/Creating_DataBase.sql
+++ RNA_Bank/Creating_DataBase.sql
@@ -3,11 +3,12 @@
 
 -- # Using RNA_Bank_DB Daabase
 use RNA_Bank_DB;
+show tables;
 
 -- # Customer Personal Info
 create table customer_personal_info
 (
-	customer_id varchar(10),
+	customer_ID varchar(10) not null,
     customer_name varchar (20),
     date_of_birth date,
     gurdian_name varchar(30),
@@ -19,5 +20,57 @@ create table customer_personal_info
     identification_doc_type varchar(20),
     id_doc_no varchar(20),
     citizenship varchar(20),
-    constraint cust_pers_info_pk primary key(customer_id)
+    constraint cust_pers_info_pk primary key(customer_ID)
 );
+
+create table customer_reference_info
+(
+	customer_ID varchar(10) not null,
+    reference_acc_name varchar(30),
+    reference_acc_no bigint(16),
+    reference_acc_address varchar(30),
+    relation varchar(30),
+    constraint cust_ref_info_pk primary key(customer_ID),
+    #constraint cust_ref_info_pk primary key(customer_ID)
+);
+
+create table bank_info
+(
+	ifsc_code varchar(15),
+    bank_name varchar(30),
+    branch_name bigint(16),
+    constraint bank_info_pk primary key(ifsc_code)
+);
+
+create table account_info(
+	account_no bigint(16),
+    customer_ID varchar(5),
+    account_type varchar(10),
+    registration_date date,
+    activation_date date,
+    ifsc_code varchar(10),
+    intrest decimal(7,2),
+    initial_deposit bigint(10),
+	constraint account_info_pk primary key(account_no),
+	constraint acs_info_pers_fk foreign key(account_no),
+	constraint account_info_pk primary key(account_no)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
